8f254921704ae976d2ef3c6d7b106f0ea9b61185
NestorAlan <rohastro.09@gmail.com>
2022-09-12 22:21:53 -0500

    segunda_prueba
    Se implementó el optimizador "Stochastic Gradient Descent" de modo que el valor de nuestra "fricción" sea de 0.87, y se realizaron tres pruebas para distintos valores de la tasa de aprendizaje: 3.0, 0.3 y 0.03, cada uno con porcentaje de aciertos del 36.63%, 95.08% y 93.55%

--- network.py
+++ network.py
@@ -45,7 +45,7 @@ class Network(object):
             a = sigmoid(np.dot(w, a)+b)
         return a
 
-    def SGD(self, training_data, epochs, mini_batch_size, eta,
+    def SGD(self, training_data, epochs, mini_batch_size, eta, gamma,
             test_data=None):
         """Train the neural network using mini-batch stochastic
         gradient descent.  The ``training_data`` is a list of tuples
@@ -69,27 +69,30 @@ class Network(object):
                 training_data[k:k+mini_batch_size]
                 for k in range(0, n, mini_batch_size)] 
             for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
+                self.update_mini_batch(mini_batch, eta, gamma)
             if test_data:
                 print("Epoch {} : {} / {}".format(j,self.evaluate(test_data), n_test))
             else:
                 print("Epoch {} complete".format(j))
 
-    def update_mini_batch(self, mini_batch, eta):
+    def update_mini_batch(self, mini_batch, eta, gamma):
         """Update the network's weights and biases by applying
         gradient descent using backpropagation to a single mini batch.
         The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
         is the learning rate."""
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
+        aux_b = [np.zeros(b.shape) for b in self.biases]
+        aux_w = [np.zeros(w.shape) for w in self.weights]
         for x, y in mini_batch:
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.weights = [w-(eta/len(mini_batch))*nw
-                        for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b-(eta/len(mini_batch))*nb
-                       for b, nb in zip(self.biases, nabla_b)]
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+
+        aux_b = [(-1) * nabla_b + (1 - gamma) * aux_b for nabla_b, aux_b in zip(nabla_b, aux_b)]
+        aux_w = [(-1) * nabla_w + (1 - gamma) * aux_w for nabla_w, aux_w in zip(nabla_w, aux_w)]
+        self.weights = [w + (eta * item_aux_w) for w, item_aux_w in zip(self.weights, aux_w)]
+        self.biases = [b + (eta * item_aux_b) for b, item_aux_b in zip(self.biases, aux_b)]
 
     def backprop(self, x, y):
         """Return a tuple ``(nabla_b, nabla_w)`` representing the
